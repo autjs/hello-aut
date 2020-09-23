4bae8eead111c37bc2472e12bfd0a3ca02d223cc
luochen <lc@bcxs.com>
2020-09-23 18:17:31 +0800

    Git灏忓姪鎵婡钀藉皹

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import ez from 'ezdo'
+import './base/reset.css'
+import Home from './pages/index'
+import Router from 'ezdo-router'
+
+
+// let router = new Router({
+//     view: new Home(),
+//     routes: [
+//         { path: '/', tem: '' },
+//         { path: '/a', tem: '' },
+//         { path: '/a/b', tem: '' },
+//         { path: '/b', tem: '' },
+//     ]
+// })
+// router.path = '/'
+
+// ez.use(router)
+
+//
+ez.render(new Home())
+
